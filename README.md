392460bc85a9bfd75a37f4dfeba3971acbfdd7d0
Brad Martin <Brad Martin>
2015-12-11 10:34:11 -0600

    testign a Promise for the action. method, renamed the methods (simple,action) and allowing argument on .action to be an options object

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ A NativeScript plugin providing the Material Design SnackBar.
 ```XML
 <Page>
    <StackLayout>
-     <Button text="Show Simple Snack" tap="simple" />
-     <Button text="Show Action Snack" tap="action" />
+     <Button text="Show Simple Snack" tap="simpleSnack" />
+     <Button text="Show Action Snack" tap="actionSnack" />
    </StackLayout>
 </Page>
 ```
@@ -20,7 +20,6 @@ A NativeScript plugin providing the Material Design SnackBar.
 ```JavaScript
 var snackbar = require("nativescript-snackbar");
 
-// 0.0.1 will use the page variable from the pageLoaded event
 // Future enhancement is to remove this argument from the API
 var page;
 
@@ -29,31 +28,43 @@ exports.loaded = function(args) {
    page = args.object;
 };
 
-// .simpleSnackbar(View page, string snackText)
-exports.simple = function(args) {
+exports.simpleSnack = function(args) {
+
    var snackText = "I Love NativeScript!";
-   //Call the simpleSnackbar() method on the nativescript-snackbar module
-   snackbar.simpleSnackbar(page, snackText);
+      
+   //Call the simple() method on the nativescript-snackbar module
+   snackbar.simple(page, snackText);
+   
 };
 
-// .actionSnackbar(View page, string snackText, int hideDelay, string actionText, function actionTextClickFunction)
-exports.action = function(args) {
+exports.actionSnack = function(args) {
+
+   var options = {};
+   options.page = page;
 
    // The snackText here is the main text to display with the snackbar, per the Material Design spec this should be a short message.
    var snackText = "Emails Deleted.";
+   options.snackText = snackText;
    
    // The android snackbar has a default of 3000ms before it hides (animates out of view)
    var hideDelay = 3000;
+   options.hideDelay = 3000;
    
    // The actionText will appear on the right side and is interactive if the user taps it before the snackbar hides
    var actionText = "UNDO";
+   options.actionText = actionText;
    
    // Action text click function
    var actionTextClickFunction = function () {
       alert('Action clicked.');
    };
+   options.actionTextClickFunction = actionTextClickFunction;   
    
-   // Call the .actionSnackbar() method on the nativescript-snackbar module
-   snackbar.actionSnackbar(page, snackText, hideDelay, actionText, actionTextClickFunction);
+   // Call the .action() method on the nativescript-snackbar module
+   snackbar.action(options).then(function(r) {
+        console.log("Result: " r);
+   }, function(err) {
+        console.log("Error: " err);
+   });
    
 };
--- index.js
+++ index.js
@@ -1,57 +1,68 @@
 var app = require("application");
+var result;
 
-exports.actionSnackbar = function(page, snackText, hideDelay, actionText, actionClickFunction) {
-    // Just making sure we are on Android for the native approach
-    // Will see about finding someone to help with iOS side
-    if (app.android) {
-        
-        // Make sure user sent actionText and actionClickFunction
-        // if undefined then we will pass the page, snackText arguments to the simpleSnackbar() method.
-        if (!actionText && !actionClickFunction) {
-          exports.simpleSnackbar(page, snackText);  
-        } else {
-              var listener;
-            
-             // First, lets create the OnClickListener callback for the Action of the Snackbar
-            if (actionClickFunction) {
-                listener = new android.view.View.OnClickListener({
-                  onClick: actionClickFunction
-                });
-            }
-        
-            // Check for hideDelay, actionText
-            if (!hideDelay) {
-                hideDelay = 3000;
-            };
-            
-            if (!actionText) {
-                actionText = "";
-            };
-        
-            // Create the native snackbar
-             var snackbar = android.support.design.widget.Snackbar;
-             
-            // Use the .make(), .setAction() methods to add text and functionality to the snackbar.
-            snackbar.make(page.android, text, delay)
-                .setAction(actionText, listener)
-                .show();
-                
-        };
-        
-    }
-};
-
-// .simpleSnackbar(View page, string snackText) is the simplest method available to construct a native snackbar
-exports.simpleSnackbar = function(page, snackText) {
+// .simple(View page, string snackText) is the simplest method available to construct a native snackbar
+exports.simple = function(page, snackText) {
     
     if (page.android && snackText) {
         
         // Create the native snackbar
-        var snackbar = android.support.design.widget.Snackbar;
-        
+        var snackbar = android.support.design.widget.Snackbar;        
         // Now call the snackbar .make() and .show() methods
         snackbar.make(page.android, snackText, 3000)
-                .show();
+                .show();                
+    }    
+};
+
+
+
+// exports.actionSnackbar = function(page, snackText, hideDelay, actionText, actionClickFunction) {
+exports.action = function(options) {
+    return new Promise(function(resolve, reject) {
+        try {
+              // Just making sure we are on Android for the native approach
+              // Will see about integrating a cocoapod for iOS version
+              if (app.android) {
+        
+              // Make sure user sent actionText and actionClickFunction
+              // if undefined then we will pass the page, snackText arguments to the simpleSnackbar() method.
+              if (!actionText && !actionClickFunction) {
+                    exports.simpleSnackbar(page, snackText);  
+               } else {
+              var listener;
+            
+              // First, lets create the OnClickListener callback for the Action of the Snackbar
+              if (actionClickFunction) {
+                  listener = new android.view.View.OnClickListener({
+                    onClick: actionClickFunction
+                  });
+              }
+        
+              // Check for hideDelay, actionText
+              if (!hideDelay) {
+                  hideDelay = 3000;
+              };
+            
+              if (!actionText) {
+                  actionText = "";
+              };
+        
+              // Create the native snackbar
+              var snackbar = android.support.design.widget.Snackbar;
+             
+              result = {};
+              result.resolve = resolve;
+            
+             // Use the .make(), .setAction() methods to add text and functionality to the snackbar.
+             result.dialog =  snackbar.make(page.android, tenxt, delay)
+                                        .setAction(actionText, listener)
+                                        .show();
                 
-    };
+        };
+        
+    }
+        } catch(ex) {
+            reject(ex);
+        }
+    });
 };
