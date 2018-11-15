995f193ee6d16e80c79e941bfeadd8631dff31eb
Brad Martin <bradwaynemartin@gmail.com>
2018-11-14 18:03:49 -0600

    add common file

---
+++ src/snackbar.common.ts
@@ -0,0 +1,51 @@
+export interface SnackBarOptions {
+  /**
+   * The action button text of the snackbar.
+   */
+  actionText: string;
+
+  /**
+   * The text of the snackbar.
+   */
+  snackText: string;
+
+  /**
+   * Delay to hide the snackbar.
+   */
+  hideDelay: number;
+
+  /**
+   * Action Text Color of the snackbar.
+   */
+  actionTextColor?: string;
+
+  /**
+   * Text Color of the snackbar.
+   */
+  textColor?: string;
+
+  /**
+   * Background Color of the snackbar.
+   */
+  backgroundColor?: string;
+  /**
+   * *Android Only*
+   * Set the maxLines if you are displaying a long string of text and it will wrap.
+   */
+  maxLines?: number;
+
+  /**
+   * *Android Only*
+   * Use RTL for textview of snackbar.
+   */
+  isRTL?: boolean;
+}
+
+export enum DismissReasons {
+  SWIPE = 'Swipe',
+  ACTION = 'Action',
+  TIMEOUT = 'Timeout',
+  MANUAL = 'Manual',
+  CONSECUTIVE = 'Consecutive',
+  UNKNOWN = 'Unknown'
+}
