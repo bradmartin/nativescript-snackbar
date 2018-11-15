b0d58b22bc19a3e5b113022c94b3eb4fde6e76e4
Brad Martin <bradwaynemartin@gmail.com>
2018-11-14 18:04:15 -0600

    update demo to 5x

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,18 +1,11 @@
-import * as app from 'tns-core-modules/application';
 import { Color } from 'tns-core-modules/color';
 import { EventData } from 'tns-core-modules/data/observable';
-import { device, isAndroid } from 'tns-core-modules/platform';
 import { Page } from 'tns-core-modules/ui/page';
 import { HelloWorldModel } from './main-view-model';
 
 // Event handler for Page "loaded" event attached in main-page.xml
 export function pageLoaded(args: EventData) {
-  // Get the event sender
-  let page = <Page>args.object;
+  let page = args.object as Page;
   page.bindingContext = new HelloWorldModel(page);
-
-  if (isAndroid && device.sdkVersion >= '21') {
-    const window = app.android.startActivity.getWindow();
-    window.setStatusBarColor(new Color('#d3d3d3').android);
-  }
+  page.androidStatusBarBackground = new Color('#5812a8');
 }
--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -8,33 +8,54 @@ export class HelloWorldModel extends Observable {
   public actionText: string;
   public jsonResult: string;
 
+  private _colors = [
+    '#c61b49',
+    '#fff000',
+    '#336699',
+    '#f47742',
+    '#047c3a',
+    '#066c7a',
+    '#ed0909',
+    '#467c1a'
+  ];
+
   constructor(mainPage: Page) {
     super();
-
     this._SnackBar = new SnackBar();
     this.snackText =
-      'Snackbar Text on a really freaking lone max line number, this is only 3 max lines but it could be more :) so I will keep rambling on and on and on and on until the cows come home. Hope this works well.';
+      'Snackbar Text on a really long max line number, this is only 3 max lines but it could be more :) so I will keep rambling on and on and on and on. Hope this works well.';
     this.actionText = 'Okay';
     this.jsonResult = '';
   }
 
   public showSimple() {
-    this._SnackBar.simple(this.get('snackText'), null, null, 3, true).then(args => {
-      this.set('jsonResult', JSON.stringify(args));
-    });
+    this._SnackBar
+      .simple('Hello from the 🍫 SnackBar 👋')
+      .then(args => {
+        console.log('SnackBar.simple() result', args);
+        this.set('jsonResult', JSON.stringify(args));
+      })
+      .catch(error => {
+        console.log('simple snackbar error', error);
+      });
   }
 
   public showAction() {
     let options: SnackBarOptions = {
       actionText: this.get('actionText'),
-      textColor: '#333',
-      actionTextColor: '#ff4081',
-      backgroundColor: '#d3c454',
+      textColor: '#fff',
+      actionTextColor: this._colors[
+        Math.floor(Math.random() * this._colors.length)
+      ],
+      backgroundColor: this._colors[
+        Math.floor(Math.random() * this._colors.length)
+      ],
       snackText: this.get('snackText'),
       hideDelay: 3500
     };
 
     this._SnackBar.action(options).then(args => {
+      console.log('SnackBar.action() result', args);
       if (args.command === 'Action') {
         this.set('jsonResult', JSON.stringify(args));
       } else {
